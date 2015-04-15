219315a7ce87312848db769fc9f12eb1e5fba269
worklxd <sj_lxd@sjtu.edu.cn>
2015-04-15 19:18:02 +0800

    table view some buttons

--- mainwindow.h
+++ mainwindow.h
@@ -2,25 +2,46 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QStandardItemModel>
+#include <QtNetwork/QUdpSocket>
 namespace Ui {
 class MainWindow;
 }
+
+enum MessageType{Message, NewParticipant, PariticipantLeft, FileName, Refuse};
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    QStandardItemModel *model;
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-private slots:
-    void on_scanButton_clicked();
+protected:
+    //void newParticipant(QString localHostName, QString ipAdress);
+    //void participantLeft(QString localHostName, QString ipAdress);
+    void sendMsg(MessageType type, QString serverAdress);
+    void keyPressEvent(QKeyEvent *event);
+
+    QString getIp();
+    QString getMessage();
 
 private:
     Ui::MainWindow *ui;
+    QString localIp;
+    QUdpSocket *udpSocket;
+    qint16 port;
+    qint16 peopleNums;
     void scanOnlineusers();
+    void setUserList();
+    void UdpInit();
+
+private slots:
+    void processPendingDatagrams();
+    void on_sendButton_clicked();
+
 };
 
 #endif // MAINWINDOW_H
